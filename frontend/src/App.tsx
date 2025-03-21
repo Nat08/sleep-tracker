ded052a0841f425b88aea01123734ade4e23ec80
Nat08 <nathan.pyanin@gmail.com>
2025-03-20 17:05:39 -0700

    Add template to active sleep tracker tab

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,26 +3,32 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Alert from '@mui/material/Alert';
-import { Box, Button, Paper, Stack } from '@mui/material';
+import { Box, Button, Paper, Stack, Typography } from '@mui/material';
 import TimerIcon from '@mui/icons-material/Timer';
 import EqualizerIcon from '@mui/icons-material/Equalizer';
 
 function App() {
   return (
     <>
-      <Paper elevation={3} className='app-frame'>
-        <Stack>
-          <Box height={'20%'} width={'100%'}>
+      <Paper elevation={2} className='app-frame'>
+        <Stack direction={'column'} height={'100%'} sx={{flex: '1'}}>
+          <Box height={'10%'} width={'100%'} sx={{'border-bottom': 'solid 1px black'}}>
             <Stack direction='row-reverse' >
               <Button>
-                <TimerIcon/>
+                <TimerIcon sx={{width: '30px', height: '30px'}}/>
               </Button>
               <Button>
-                <EqualizerIcon/>
+                <EqualizerIcon sx={{width: '30px', height: '30px'}}/>
               </Button>
             </Stack>
           </Box>
-          <Box bgcolor={'blue'} sx={{height: '100%', width: '100%'}}>
+          <Box sx={{height: '90%', width: '100%'}}>
+            <Stack direction={'column'} alignItems='center' alignContent='center' justifyContent='center' sx={{flex: '1'}} height={'100%'} spacing={10}>
+                <Typography variant='h2'>01:10:36</Typography>
+                <Button variant="contained" size='large'>
+                  Stop
+                </Button>
+            </Stack>
           </Box>
         </Stack>
       </Paper>
