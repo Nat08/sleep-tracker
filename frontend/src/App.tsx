a21f4dc1cebfd0c0e9995201c5f20f42c3c16891
Nat08 <nathan.pyanin@gmail.com>
2025-04-15 17:57:47 -0700

    Add start and stop button interaction and timer display

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,8 +6,40 @@ import Alert from '@mui/material/Alert';
 import { Box, Button, Paper, Stack, Typography } from '@mui/material';
 import TimerIcon from '@mui/icons-material/Timer';
 import EqualizerIcon from '@mui/icons-material/Equalizer';
+import prettyMs from 'pretty-ms';
 
 function App() {
+
+  const [isActiveSession, setIsActiveSession] = useState(false);
+  const [startTime, setStartTime] = useState(new Date());
+  const [endTime, setEndTime] = useState(new Date(startTime));
+  const [duration, setDuration] = useState(0);
+  const [stopWatchId, setStopWatchId] = useState(0);
+
+  useEffect(() => {
+    if (!isActiveSession) {
+      clearInterval(stopWatchId);
+      return;
+    }
+    
+    const currentTime = new Date();
+    setStartTime(currentTime);
+    setEndTime(currentTime);
+
+    const intervalId = setInterval(() => {
+      setEndTime(endTime => {
+        const currentTime = endTime.getTime();
+        return new Date(currentTime + 1000)
+      })
+    }, 1000);
+
+    setStopWatchId(intervalId);
+  }, [isActiveSession])
+
+  useEffect(() => {
+    setDuration(endTime.getTime() - startTime.getTime());
+  }, [endTime])
+
   return (
     <>
       <Paper elevation={2} className='app-frame'>
@@ -24,9 +56,11 @@ function App() {
           </Box>
           <Box sx={{height: '90%', width: '100%'}}>
             <Stack direction={'column'} alignItems='center' alignContent='center' justifyContent='center' sx={{flex: '1'}} height={'100%'} spacing={10}>
-                <Typography variant='h2'>01:10:36</Typography>
-                <Button variant="contained" size='large'>
-                  Stop
+                <Typography variant='h2'>{prettyMs(duration)}</Typography>
+                <Button variant="contained" size='large' onClick={(_) => {
+                    setIsActiveSession(!isActiveSession)
+                  }}>
+                  {isActiveSession ? 'Stop' : 'Start'}
                 </Button>
             </Stack>
           </Box>
